2b8cb5a08f86eb82f7e24dfeb29c167ac1c10e8f
wyl19 <william_ldna@hotmail.com>
2024-08-23 23:38:58 -0300

    Desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -15,16 +17,16 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            
+           
+            if(Suite.Capacidade >= hospedes.Count )
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                
+                throw new Exception("Não possui quartos para atender a demanda de Hospedes");
             }
         }
 
@@ -35,23 +37,20 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            
+            
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+          
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = (valor * 0.9m);
             }
 
             return valor;
